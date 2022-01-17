cd2dad983bfbbd089663950c4954bdafc8ca7d17
Alonso Diaz <adiaz58@ucsc.edu>
2022-01-16 16:41:45 -0800

    edit startup

--- events/ready.js
+++ events/ready.js
@@ -11,86 +11,18 @@ const adminPermissions = [
 ];
 
 module.exports = async (client) => {
-	let count = 0;
 	if (!client.application?.owner) client.application?.fetch();
 
 	client.ignoreList = await botUtils.getIgnoreList(client);
 
 	if (!config.globalCommands) {
-		client.guilds.cache
-			.map((g) => g)
-			.forEach((guild) => {
-				count++;
-				// update guild config in database
-				client.db.GuildConfig.findOne({ where: { guildId: guild.id } })
-					.then((token) => {
-						if (!token) {
-							client.db.GuildConfig.create({
-								guildName: guild.name,
-								guildId: guild.id,
-								prefix: config.prefix,
-								adminRoles: '',
-								modRoles: '',
-								ownerId: config.adminId,
-							});
-						}
-					})
-					.then(() => {
-						// update permissions
-						guild?.commands
-							.fetch()
-							.then((guildCommands) => {
-								if (guildCommands) {
-									guildCommands.forEach((c) => {
-										if (client.adminSlashCommands.includes(c.name)) {
-											c.permissions.add({ permissions: adminPermissions });
-										}
-									});
-								}
-							})
-							.catch(console.error);
-					})
-					.catch((e) => console.log(e));
-
-				guild.commands.set(client.slash).catch((e) => console.log(e));
-
-				// Clear counting mute users
-				client.db.Count.findOne({ where: { guildId: guild.id } }).then((t) => {
-					if (!t) return;
-					if (!t.channelId || !t.countingMute) return;
-
-					guild.channels.fetch(t.channelId).then((c) => {
-						c.messages.fetch({ limit: 1 }).then((m) => {
-							const msg = m.first();
-							const numMatch = msg.content.match(/^([1-9]\d*)/);
-
-							if (numMatch && parseInt(numMatch[1]) != t.lastNumber) {
-								console.log(
-									`Last number updated for ${guild.name} [${t.lastNumber} => ${numMatch[1]}]`,
-								);
-								client.db.Count.update(
-									{ lastNumber: numMatch[1], lastMember: msg.author.id },
-									{ where: { guildId: guild.id } },
-								);
-							}
-						});
-					});
-
-					guild.members.fetch().then(() => {
-						guild.roles.fetch(t.countingMute).then((r) => {
-							// console.log(r.members);
-							r.members.forEach((m) => {
-								m.roles.remove(r);
-							});
-						});
-					});
-				});
-			});
-		console.log(color.yellow('Removed counting mute roles'));
-		// set slash commands
+		await setup(client);
 	}
+
 	console.log(
-		color.yellow(`Set commands/permissions/config for ${count} guilds`),
+		color.yellow(
+			`Set commands/permissions/config for ${client.guilds.cache.size} guilds`,
+		),
 	);
 
 	const stop = Date.now();
@@ -102,3 +34,72 @@ module.exports = async (client) => {
 		),
 	);
 };
+
+async function setup(client) {
+	for (const guild of client.guilds.cache.map((g) => g)) {
+		// update guild config in database
+		const token = await client.db.GuildConfig.findOne({
+			where: { guildId: guild.id },
+		});
+
+		if (!token) {
+			await client.db.GuildConfig.create({
+				guildName: guild.name,
+				guildId: guild.id,
+				prefix: config.prefix,
+				adminRoles: '',
+				modRoles: '',
+				ownerId: config.adminId,
+			});
+		}
+
+		// update permissions
+		await guild.commands
+			.fetch()
+			.then((guildCommands) => {
+				if (guildCommands) {
+					guildCommands.forEach((c) => {
+						if (client.adminSlashCommands.includes(c.name)) {
+							// edit permissions here
+							c.permissions.add({ permissions: adminPermissions });
+						}
+					});
+				}
+			})
+			.catch(console.error);
+
+		guild.commands.set(client.slash).catch((e) => console.log(e));
+
+		// Clear counting mute users
+		await guild.members.fetch();
+		client.db.Count.findOne({ where: { guildId: guild.id } }).then((t) => {
+			if (!t) return;
+			if (!t.channelId || !t.countingMute) return;
+
+			guild.channels.fetch(t.channelId).then((c) => {
+				c.messages.fetch({ limit: 1 }).then((m) => {
+					const msg = m.first();
+					const numMatch = msg.content.match(/^([1-9]\d*)/);
+
+					if (numMatch && parseInt(numMatch[1]) != t.lastNumber) {
+						console.log(
+							`Last number updated for ${guild.name} [${t.lastNumber} => ${numMatch[1]}]`,
+						);
+						client.db.Count.update(
+							{ lastNumber: numMatch[1], lastMember: msg.author.id },
+							{ where: { guildId: guild.id } },
+						);
+					}
+				});
+			});
+
+			// clear members with counting mute role
+			guild.roles.fetch(t.countingMute).then((r) => {
+				r.members.forEach((m) => {
+					m.roles.remove(r);
+				});
+			});
+		});
+	}
+	console.log(color.yellow('Removed counting mute roles'));
+}
